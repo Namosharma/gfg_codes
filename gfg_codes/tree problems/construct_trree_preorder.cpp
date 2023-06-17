6439581ab5ea1ae9e831ea298b1c7b1124c88dd7
Namosharma <134762253+Namosharma@users.noreply.github.com>
2023-06-17 18:53:35 +0530

    leaf help
    leaf help

---
+++ gfg_codes/tree problems/construct_trree_preorder.cpp
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    data = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+void printInorder (struct Node* node)
+{
+	if (node == NULL)return;
+	printInorder(node->left);
+	printf("%d ", node->data);
+	printInorder (node->right);
+}
+
+Node* constructTree(int n, int pre[], char preLN[]);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        Node* root = NULL;
+        int n;
+        cin>>n;
+        int pre[n];
+        char preLN[n];
+        for(int i=0; i<n; i++)cin>>pre[i];
+        for(int i=0; i<n; i++)cin>>preLN[i];
+        root = constructTree (n, pre, preLN);
+        printInorder(root);
+        cout<<endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/*Structure of the Node of the binary tree is as
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+};
+*/
+// function should return the root of the new binary tree formed
+Node*build(int pre[],int &i,int n,char leaf[]){
+    if(i>=n){
+        return NULL;
+    }
+    if(leaf[i]=='L'){
+        
+        Node*n=new Node(pre[i++]);
+        return n;
+    }
+    Node*root=new Node(pre[i++]);
+    root->left=build(pre,i,n,leaf);
+    root->right=build(pre,i,n,leaf);
+    return root;
+}
+struct Node *constructTree(int n, int pre[], char preLN[])
+{
+    // Code here
+    int i=0;
+    return build(pre,i,n,preLN);
+}
